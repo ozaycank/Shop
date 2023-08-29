faf1493c5cde25a852f7ce90d584eb50acdbfc57
Özay Can Kırlı <ozaycank97@gmail.com>
2023-08-29 16:56:49 +0300

    Built-in and Custom Pipes added

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,5 +9,6 @@ import { Product } from './product';
 })
 export class ProductComponent {
   title="Product List"
+  filterText = ""
   products :Product[] = [{id:1,name:"Laptop",price:10000,categoryId:1,description:"Lenovo Legion",imageUrl:""},{id:2,name:"Camera",price:2000,categoryId:2,description:"A4 Tech",imageUrl:""}]
 }
