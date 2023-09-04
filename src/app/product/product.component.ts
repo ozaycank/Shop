33c02b3e691915326ea73689072fa22581158a42
Özay Can Kırlı <ozaycank97@gmail.com>
2023-09-04 16:35:23 +0300

    Event binding and alertifyjs added

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
+declare let alertify:any;
 
 @Component({
   selector: 'app-product',
@@ -7,8 +8,16 @@ import { Product } from './product';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent{
+  constructor(){}
   title="Product List"
   filterText = ""
   products :Product[] = [{id:1,name:"Laptop",price:10000,categoryId:1,description:"Lenovo Legion",imageUrl:""},{id:2,name:"Camera",price:2000,categoryId:2,description:"A4 Tech",imageUrl:""}]
+}
+  addToCart(product){
+  alertify.success(product.name +"Added to Cart!")
+}
+
+function addToCart() {
+  throw new Error('Function not implemented.');
 }
